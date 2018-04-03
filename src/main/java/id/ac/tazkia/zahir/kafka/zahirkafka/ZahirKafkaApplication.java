9a3d1fc1e6f281a6db34b2d2a26162b206693571
Endy Muhardin <endy.muhardin@gmail.com>
2018-04-03 10:53:39 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/zahir/kafka/zahirkafka/ZahirKafkaApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.zahir.kafka.zahirkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZahirKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZahirKafkaApplication.class, args);
+	}
+}
