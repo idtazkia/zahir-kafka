78eda3a0617cd74bc04117e823597a385a99d1b1
Endy Muhardin <endy.muhardin@gmail.com>
2018-04-24 16:46:22 +0700

    disable webserver

--- src/main/java/id/ac/tazkia/zahir/ZahirKafkaApplication.java
+++ src/main/java/id/ac/tazkia/zahir/ZahirKafkaApplication.java
@@ -1,12 +1,14 @@
 package id.ac.tazkia.zahir;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZahirKafkaApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ZahirKafkaApplication.class, args);
+		SpringApplication app = new SpringApplication(ZahirKafkaApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 }
