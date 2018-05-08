b83a6467741f4d3a0941e10c84b3600c5efb24f1
Endy Muhardin <endy.muhardin@gmail.com>
2018-05-08 15:04:08 +0700

    initial data

--- src/main/resources/db/migration/V1.0.0.2018041902__Initial_Data.sql
+++ src/main/resources/db/migration/V1.0.0.2018041902__Initial_Data.sql
@@ -1,7 +1,7 @@
 insert into invoice_configuration(id, invoice_type, department, product, customer)
 values (
 'pmb2017','pmb2017',
-'7a9112ef-70a0-408f-89bb-efd7dbe0fc73','b07129d6-dd9b-47c4-a6b6-cba9c6680940',
+'7a9112ef-70a0-408f-89bb-efd7dbe0fc73','f779a529-22f7-4b26-933a-4b26d400af23',
 'e7d73eb7-25de-48a0-b1a3-f0b6f9b2416f');
 
 insert into bank (id, account_name, account_number, account_code)
@@ -10,5 +10,23 @@ values ('bsm001', 'BSM', '111200600', 'df81d233-2f7e-4a27-86dc-d8d161abd33b');
 insert into bank (id, account_name, account_number, account_code)
 values ('bnisy001', 'BNI Syariah', '111200400', 'b4f6003c-4c0e-4a2e-8b20-9d36507b3c34');
 
-insert into invoice(id, sales_invoice_number, invoice_number, customer, amount, invoice_status)
-values ('719eac42-5ef5-470c-92a5-46b51716564e', '00000087', '2018042301000003', 'e7d73eb7-25de-48a0-b1a3-f0b6f9b2416f', 300000.00, 'UNPAID');
+insert into project(id, code, name )
+values ('4b7a4bd6-5269-49f4-a955-b34eccfd390c', 'PSBM-001', 'PRODI S1 BISNIS MANAJEMEN SYARIAH');
+
+insert into project(id, code, name )
+values ('f2d34f9d-9a8c-446f-83bc-d7c5efa8a066', 'PSAS-001', 'PRODI S1 AKUNTANSI SYARIAH');
+
+insert into project(id, code, name )
+values ('380ea253-292f-4345-b6cc-b15c60b112ae', 'PSES-001', 'PRODI S1 EKONOMI SYARIAH');
+
+insert into project(id, code, name )
+values ('9b0e1543-327a-455f-adec-838c40cb34a2', 'PSHES-001', 'PRODI S1 HUKUM EKONOMI SYARIAH');
+
+insert into project(id, code, name )
+values ('013b9c7c-4d70-4383-9ba4-c587e2cdc211', 'PSMES-001', 'PRODI S2 MAGISTER EKONOMI SYARIAH');
+
+insert into project(id, code, name )
+values ('d197cc2a-37f4-4985-8933-07e56b7eb08b', 'PSTI-001', 'PRODI S1 TADRIS IPS');
+
+insert into project(id, code, name )
+values ('81d8ed24-7084-465c-97a5-5b59650e0214', 'M-001', 'Matrikulasi');
